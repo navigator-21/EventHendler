21e3e8cbb23a313513e6e7561e9db0b66de3d197
Roman <63311042+romantynyanov@users.noreply.github.com>
2021-06-16 13:42:57 +0300

    events.php

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # EventHendler
 An example of creating events in a PHP executable
+
+Это простой пример создания обработчика событий в исполняемых функций на PHP.
+
+Колличество событий не ограничено. Однако в приведенном примере расмотрим только три базовых.
+[
+   "onBefore"=> "Before processing input parameters.",        
+   "onActive"=>"After processing the input parameters just before executing the main code.",
+   "onAfter" => "After executing all the code, before returning the parameters.",
+]
+
--- index.php
+++ index.php
@@ -1,48 +1,5 @@
 <?  
 
-class Events 
-{
-
-    static public $arEventsList = array(
-        "onBefore"=> "Before processing input parameters.",        
-        "onActive"=>"After processing the input parameters just before executing the main code.",
-        "onAfter" => "After executing all the code, before returning the parameters.",
-    );
-
-    static public $arEvents = array();
-
-    //Registers events (callback functions).
-    static function AddEventHandler($module, $event,$class,$method, $fun)
-    {
-        $classOn = mb_convert_case(trim($class), MB_CASE_TITLE, 'UTF-8');
-        $methodOn = mb_convert_case(trim($method), MB_CASE_TITLE, 'UTF-8');        
-        self::$arEvents[trim($module)][trim($event).$classOn.$methodOn] = $fun;       
-    }
-
-    //Handles events
-    static function doEvent($module, $event, $class,$method, array &$data = null)
-    {       
-        $eventsMod = self::$arEvents[trim($module)];
-        $doEvent = trim($event);
-        $doClass = mb_convert_case(trim($class), MB_CASE_TITLE, 'UTF-8');
-        $doMethod = mb_convert_case(trim($method), MB_CASE_TITLE, 'UTF-8');
-        $strEvent = $doEvent.$doClass.$doMethod; 
-        
-        foreach ($eventsMod as $k=>$f)
-        {               
-            if ($strEvent == $k)
-            {
-                if (!is_callable ($f))
-                {
-                    throw new Exception("The callback function is not callable!"); 
-                }                    
-                //$result = call_user_func($fun, $data);
-                $result =$f($data);
-                if (isset($result))return $result;                
-            }
-        }
-    }
-}
 
 class Db 
 {
